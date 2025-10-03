ab35158cb0b9aa8227fb1b46b71e15fd195ea07a
sangwookpark <71429903+sangwookp9591@users.noreply.github.com>
2025-10-03 19:25:39 +0900

    fix: Remove unused import and adjust whitespace in UserController

--- src/main/java/com/ecommerce/domain/user/controller/UserController.java
+++ src/main/java/com/ecommerce/domain/user/controller/UserController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -69,7 +68,7 @@ public class UserController {
 
         return ApiResponse.success("조회 성공", response);
     }
-    
+
     /**
      * 이메일로 사용자 조회
      * 
